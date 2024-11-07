08e85acbf3c81691116fb6c9324059e91abd163a
kirillio25 <this.kirill.ionkin@gmail.com>
2024-11-07 21:27:56 +0500

    add gh-page2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: '/academic-performance-analysis/',
+  base: '/academic-performance-analysis/',
   plugins: [
     vue(),
   ],
